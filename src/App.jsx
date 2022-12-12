b9d05b35d5345016694a3234c47078c4c249b13e
lukasnam <my email address used on my git repo>
2022-12-12 16:03:18 -0500

    fixed and removed extra code for the navbar and app components

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Navbar from './layouts/Navbar';
 import './App.scss';
 import './_typography.scss'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Services from './pages/Services';
 import Competencies from './pages/Competencies';
-import Work from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Hero from './pages/Hero';
 import Social from './components/Social';
@@ -30,21 +28,6 @@ function App() {
         <Footer />
       </div>
       </body>
-      {/* <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/competencies' element={<Competencies />} />
-      </Routes>
-      </Router> */}
-        {/* <Routes>
-          <Route path='/' />
-          <Route path='/services' element={<Services />} />
-          <Route path='/competencies' element={<Competencies />} />
-          <Route path='/work' element={<Work />} />
-          <Route path='contact' element={<Contact />} />
-        </Routes>
-      </Router> */}
-      {/* <Navbar /> */}
     </>
   );
 }
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter, Router, Link } from 'react-router-dom';
 import './Navbar.scss';
 import './_header.scss';
 import './_logo.scss'
@@ -11,49 +10,30 @@ function Navbar() {
     <>
     <header>
     <div className='nav_list'>
-    <button class="logo_btn" tabindex="3"><img src={require("./mylogo.png")} alt="my logo" className='logo' /></button>
-    <h3 class="touch_logo">click the logo!</h3>
-        {/* <Link to="/" className="logo_btn">
-        </Link> */}
+    <button className="logo_btn" tabindex="3"><img src={require("./mylogo.png")} alt="my logo" className='logo' /></button>
+    <h3 className="touch_logo">click the logo!</h3>
      <div className="nav_list">
             <ul className='nav_list'>
-                <li className="nav-item">
-                    {/* <Link to='/' className="nav-links">
-                        Home
-                    </Link> */}
+                <li className="nav-item nav-links">
                     <a href="#home">Home</a>
                 </li> 
-                <li className="nav-item">
-                    {/* <Link to='/services' className="nav-links">
-                        Services
-                    </Link> */}
+                <li className="nav-item nav-links">
                     <a href="#services">Services</a>
                 </li>
                 <li className="nav-item">
-                    {/* <Link to='/competencies' className="nav-links">
-                        Competencies
-                    </Link> */}
                     <a href="#competencies">Competencies</a>
                 </li>
                 <li className="nav-item">
                     <a href="#portfolio">Portfolio</a>
-                    {/* // <Link to='/work' className="nav-links">
-                    //     Work
-                    // </Link> */}
                 </li> 
                 <li className="nav-item">
                     <a href="#contact">Contact</a>
-                    {/* <Link to='/contact' className="nav-links">
-                        Contact
-                    </Link> */}
                 </li> 
             </ul>
-
         </div>
         </div>
       </header>
     </>
-
   )
 }
 
